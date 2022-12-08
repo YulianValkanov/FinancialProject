918c0489b7c810047a1580d54eb2e942539919a0
iulian.valkanov@gmail.com <iulian.valkanov@gmail.com>
2022-12-08 10:35:05 +0200

    redaction user interface

--- FinancialServices/Models/LoginViewModel.cs
+++ FinancialServices/Models/LoginViewModel.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinancialServices.Models
 {
     public class LoginViewModel
     {
         [Required]
+        [DisplayName("Потребителско име")]
         public string UserName { get; set; } = null!;
 
         [Required]
         [DataType(DataType.Password)]
+        [DisplayName("Парола")]
         public string Password { get; set; } = null!;
     }
 }
--- FinancialServices/Models/RegisterViewModel.cs
+++ FinancialServices/Models/RegisterViewModel.cs
@@ -1,25 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinancialServices.Models
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required]      
         [StringLength(20, MinimumLength = 5)]
         public string UserName { get; set; } = null!;
 
         [Required]
         [EmailAddress]
-        [StringLength (60, MinimumLength = 10)]
+        [StringLength (60, MinimumLength = 10)]   
         public string Email { get; set; } = null!;
 
         [Required]
         [StringLength(20, MinimumLength = 5)]
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password)]       
         public string Password { get; set; } = null!;
 
         [Compare(nameof(Password))]
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password)]       
         public string ConfirmPassword { get; set; } = null!;
     }
 }
